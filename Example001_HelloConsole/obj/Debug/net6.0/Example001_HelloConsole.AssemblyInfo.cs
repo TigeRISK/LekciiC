a85680f92450b3abcfd064cb1f1f6c099c3308b5
Marat <tigerplyus@mail.ru>
2022-07-13 22:27:09 +0300

    Example001 Hello Console

--- Example001_HelloConsole/obj/Debug/net6.0/Example001_HelloConsole.AssemblyInfo.cs
+++ Example001_HelloConsole/obj/Debug/net6.0/Example001_HelloConsole.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Exampe001_HelloConsole")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Exampe001_HelloConsole")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Exampe001_HelloConsole")]
+[assembly: System.Reflection.AssemblyProductAttribute("Example001_HelloConsole")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Example001_HelloConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
